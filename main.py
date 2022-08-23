38dad1d2c2ed4a20fccfb0b7fb9d7f6f79d618aa
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-23 09:08:21 -0400

    took data from sample csv

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
-import data
-grades = data.grades
-import matplotlib.pyplot as plt
+# import data
+# grades = data.grades
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# math = []
+# science = []
+# english = []
+# compSci=[]
+#
+# for i in grades.values():
+#    math.append(i["math"])
+#    science.append(i["science"])
+#    english.append(i["english"])
+#    compSci.append(i["computer science"])
+#
+# x=np.array(science)
+# y=np.array(compSci)
+#
+# a, b = np.polyfit(x, y, 1)
+#
+# plt.scatter(x, y)
+# plt.plot(x, a*x+b, color='blue')
+# plt.show()
+#
+
 import numpy as np
+import matplotlib.pyplot as plt
 
-math = []
-science = []
-english = []
-compSci=[]
+from pandas import read_csv
+import csv
+data = read_csv('student-mat.csv')
 
-for i in grades.values():
-   math.append(i["math"])
-   science.append(i["science"])
-   english.append(i["english"])
-   compSci.append(i["computer science"])
+absences = data['absences'].tolist()
+studyTime = data['studytime'].tolist()
 
-x=np.array(science)
-y=np.array(compSci)
-
+y=np.array(absences)
+x=np.array(studyTime)
 a, b = np.polyfit(x, y, 1)
-
 plt.scatter(x, y)
 plt.plot(x, a*x+b, color='blue')
 plt.show()
-
-
-
 
 
